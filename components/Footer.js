226f25286db5f3d2b3d8e337b8515465d4ac4aa7
Bobby Jonathan <bobby.cool00763@protonmail.com>
2022-11-25 11:37:20 +0700

    add basic structures, add react-bootstrap (#1)
    * add basic structures, add react-bootstrap
    
    * basic layout for employee page

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+import styles from '../styles/custom/Index.module.css';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>CheckPoint</p>
+      <p>Since 2022</p>
+    </footer>
+  );
+}
