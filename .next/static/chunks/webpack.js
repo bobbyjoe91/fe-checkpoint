2fdc625ed3ff98af084176174977bae64bf51993
Bobby Jonathan <bobby.cool00763@protonmail.com>
2022-11-25 11:35:22 +0700

    basic layout for employee page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0a79b74c6a7fd519"; }
+/******/ 		__webpack_require__.h = function() { return "9a031779ecf088ea"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head';
 import Image from 'next/image';
 
-import { Table } from 'react-bootstrap';
+import { Pagination, Table } from 'react-bootstrap';
 
 import Footer from '../components/Footer';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/custom/Index.module.css';
 
 export default function Home() {
   return (
@@ -24,6 +24,22 @@ export default function Home() {
           />
           <h1 className={styles.title}>John Doe</h1>
           <p>Human Resource Development | Staff</p>
+
+          {/* daterange filter */}
+          <div id="date-range-filter">
+            <p>Filter berdasarkan tanggal</p>
+            <div id='filter'>
+              <div>
+                <label className='filter-label'>Dari:</label>
+                <input type="date" />
+              </div>
+              <div style={{ width: '10px' }}/>
+              <div>
+                <label className='filter-label'>Hingga:</label>
+                <input type="date" />
+              </div>
+            </div>
+          </div>
 
           <Table striped bordered hover>
             <thead>
@@ -56,6 +72,17 @@ export default function Home() {
               </tr>
             </tbody>
           </Table>
+
+          <div className="d-flex justify-content-center">
+            <Pagination>
+              <Pagination.First />
+              <Pagination.Prev />
+              <Pagination.Item>1</Pagination.Item>
+              <Pagination.Item>2</Pagination.Item>
+              <Pagination.Next />
+              <Pagination.Last />
+            </Pagination>
+          </div>
         </main>
       </div>
 
